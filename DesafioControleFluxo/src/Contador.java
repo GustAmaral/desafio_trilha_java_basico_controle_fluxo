370462d4d6eb5a5d847a1d592bf6a5c0a96b0fc1
Gustavo <gustamaral11@gmail.com>
2025-07-16 19:05:05 -0300

    Criação da classe de excessão e criação da lógica do desafio

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int pUm = scan.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int pDois = scan.nextInt();
+
+        try {
+            contar(pUm, pDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.err.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem; i++) {
+            System.out.printf("Imprimindo o número %s\n", i + 1);
+        }
     }
 }
